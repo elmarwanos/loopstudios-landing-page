485c2c5dec64fec9c52693f0521ec2a37b1bf9eb
elmarwanos <marwanbassam13@gmail.com>
2021-01-12 20:46:22 +0200

    Completed Header including nav for mobile and desktop

---
+++ index.js
@@ -0,0 +1,18 @@
+function toggleHamburger() {
+  var links = document.getElementById("hidden-links");
+  var logo = document.getElementsByClassName("header-logo")[0];
+  var menu = document.getElementsByClassName("header-hamburger")[0];
+  if (links.style.display === "flex") {
+    links.style.display = "none";
+    logo.style.zIndex = 1;
+    menu.style.zIndex = 1;
+    menu.firstElementChild.src = "./images/icon-hamburger.svg";
+    document.body.style.overflow = "auto";
+  } else {
+    links.style.display = "flex";
+    logo.style.zIndex = 10;
+    menu.style.zIndex = 10;
+    menu.firstElementChild.src = "./images/icon-close.svg";
+    document.body.style.overflow = "hidden";
+  }
+}
