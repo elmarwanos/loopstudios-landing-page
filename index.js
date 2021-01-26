1ea33109b8398eb359f41364fd7a2fa5ac576df1
elmarwanos <marwanbassam13@gmail.com>
2021-01-26 15:18:59 +0200

    Fix the header links and Work on main intro

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ function toggleHamburger() {
   var logo = document.getElementsByClassName("header-logo")[0];
   var menu = document.getElementsByClassName("header-hamburger")[0];
   if (links.style.display === "flex") {
+    window.scrollTo(0, 0);
     links.style.display = "none";
     logo.style.zIndex = 1;
     menu.style.zIndex = 1;
     menu.firstElementChild.src = "./images/icon-hamburger.svg";
     document.body.style.overflow = "auto";
   } else {
+    window.scrollTo(0, 0);
     links.style.display = "flex";
     logo.style.zIndex = 10;
     menu.style.zIndex = 10;
